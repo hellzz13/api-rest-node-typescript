bebaca390e60bad8e5a44f69771a8ef5d4dd2f7d
hellzz13 <hpalenca13@gmail.com>
2023-08-24 07:32:41 -0300

    feat: add method getAll

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,6 +8,11 @@ router.get("/", (_, res) => {
   return res.send("Aplicação funcionando!");
 });
 
+router.get(
+  "/cidades",
+  CidadesController.getAllValidation,
+  CidadesController.getAll
+);
 router.post(
   "/cidades",
   CidadesController.createValidation,
